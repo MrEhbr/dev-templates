7946f47c2343e8eaab3724a734077aa8e6b87480
Aleksei Burmistrov <mr.ehbr@gmail.com>
2024-06-04 09:26:16 +0300

    first commit

---
+++ go/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            # goimports, godoc, formaters etc.
+            gotools
+            # Generators
+            go-minimock
+          ];
+        };
+      });
+    };
+}
